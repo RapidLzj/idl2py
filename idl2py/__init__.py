d9196dc40e8b959bf0b975301b778764395f7af3
郑捷 <lzj@RapidQ.local>
2019-04-26 19:28:24 +0800

    init lib

---
+++ idl2py/__init__.py
@@ -0,0 +1,15 @@
+"""
+A python3 library translated from IDL astron.
+By Dr. Jie Zheng, from NAOC, as a gift to Dr.Q
+v1.0 2019-04-26
+"""
+
+
+from .version import version
+
+
+
+if __name__ == "__main__":
+    print("What are you going to do with init file??")
+    print("    --> Q")
+
---
+++ idl2py/version.py
@@ -0,0 +1,10 @@
+"""
+By Dr Jie Zheng, NAOC, to Q
+v1 2019-04-27
+"""
+
+
+
+def version():
+    return "v1.0 (2019-04-27) by lzj_Q"
+
